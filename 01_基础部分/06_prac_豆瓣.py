a0464d64811d1bdfc469b7c55b7332980f017bc0
酷酷的辉 <995631664@qq.com>
2022-09-14 19:22:31 +0800

    bs4练习题答案修正

--- 01_基础部分/06_prac_豆瓣.py
+++ 01_基础部分/06_prac_豆瓣.py
@@ -5,6 +5,7 @@ book_title_intro_dict = {}
 soup = BeautifulSoup(open('../00_素材箱/豆瓣.html', 'r', True, 'utf-8'), 'lxml')
 for div in soup.select('div.detail-frame'):
     book_title_intro_dict[div.h2.a.string] = list(div.find_all('p')[2].stripped_strings)[0]
+print(book_title_intro_dict)
 
 # 获取图像链接并写入文件
 file = open('../00_素材箱/image.html', 'w', True, 'utf-8')
