3260f83859eacf637ac81ef8a7b409db4e674306
辉哥 <995631664@qq.com>
2022-10-23 21:19:40 +0800

    mysql使用云数据库，使用DictCursor查询数据

--- 05_数据库/02_mysql_查询.py
+++ 05_数据库/02_mysql_查询.py
@@ -1,14 +1,14 @@
 import pymysql
+from pymysql.cursors import DictCursor
+from pprint import pprint
 
-db = pymysql.connect(user='root', password='182182aA', host='localhost', port=3306, charset='utf8', database='test1')
-cursor = db.cursor()
+db = pymysql.connect(user='ahui', password='182182aA', host='43.138.31.29', port=3306, charset='utf8', database='my_test')
+cursor = db.cursor(DictCursor)
 
-sql = 'select 内容 from 励志文章 where ID=42'
+sql = 'select 作者,标题,时间,阅读数 from 励志文章 where ID>=3 and ID<=5'
 cursor.execute(sql)
 data = cursor.fetchall()
-for i in data:
-    for j in i:
-        print(j)
+pprint(data)
 
 cursor.close()
 db.close()
