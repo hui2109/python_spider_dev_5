459e15bfc41a00f6db5a4be3bbb0325b1ecdabcb
辉哥 <995631664@qq.com>
2022-11-06 20:12:09 +0800

    代理ip搭建课件

--- 00_素材箱/代理验证.py
+++ 00_素材箱/代理验证.py
@@ -2,8 +2,9 @@ import requests
 
 url = 'http://httpbin.org/ip'
 proxies = {
-    'http': 'http://187.190.252.248:999'
+    'http': '223.96.90.216:8085'
 }
 
+# response = requests.get(url)
 response = requests.get(url, proxies=proxies)
 print(response.text)
