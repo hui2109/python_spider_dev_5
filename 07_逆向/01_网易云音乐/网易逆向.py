978b91b6149cc0387e346bdb6d57bec0b78e3be8
酷酷的辉 <995631664@qq.com>
2023-01-01 17:35:46 +0800

    爬取网易云音乐

--- 07_逆向/01_网易云音乐/网易逆向.py
+++ 07_逆向/01_网易云音乐/网易逆向.py
@@ -5,11 +5,12 @@ import requests
 import urllib.request as urr
 
 subprocess.Popen = partial(subprocess.Popen, encoding='utf-8')
+song_id = '2010289317'
 
 _data = {
     'csrf_token': "fa77e5120e948330aa6652cdf4ba3702",
     'encodeType': "aac",
-    'ids': "[2005574298]",
+    'ids': f"[{song_id}]",
     'level': "standard"
 }
 with open('./源代码.js', 'r', True, 'utf-8') as f:
@@ -22,4 +23,4 @@ data = {
 url = 'https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token=fa77e5120e948330aa6652cdf4ba3702'
 response = requests.post(url, data=data)
 song_url = response.json()['data'][0]['url']
-urr.urlretrieve(song_url, '../00_resource/2005574298.m4a')
+urr.urlretrieve(song_url, f'../00_resource/网易音乐/{song_id}.m4a')
